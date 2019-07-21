43406592ad833dd10d82961f5f6824336c61ee94
ProfessorChristian <51305140+ProfessorChristian@users.noreply.github.com>
2019-07-21 16:26:23 +0800

    Add project files.

---
+++ EReferralSystem/Models/HomeMenuItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EReferralSystem.Models
+{
+    public enum MenuItemType
+    {
+        Home,
+        Manage,
+        Report
+    }
+    public class HomeMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ EReferralSystem/Views/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using EReferralSystem.Models;
+using EReferralSystem.Views;
+
+namespace EReferralSystem.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : MasterDetailPage
+    {
+        Dictionary<int, NavigationPage> MenuPages = new Dictionary<int, NavigationPage>();
+        public MainPage()
+        {
+            InitializeComponent();
+
+            MasterBehavior = MasterBehavior.Popover;
+
+            MenuPages.Add((int)MenuItemType.Home, (NavigationPage)Detail);
+        }
+
+        public async Task NavigateFromMenu(int id)
+        {
+            if (!MenuPages.ContainsKey(id))
+            {
+                switch (id)
+                {
+                    case (int)MenuItemType.Home:
+                        MenuPages.Add(id, new NavigationPage(new HomePage.HomePage()));
+                        break;
+                    case (int)MenuItemType.Manage:
+                        MenuPages.Add(id, new NavigationPage(new ManagePage.ManagePage()));
+                        break;
+                    case (int)MenuItemType.Report:
+                        MenuPages.Add(id, new NavigationPage(new ReportPage.ReportPage()));
+                        break;
+                }
+            }
+
+            var newPage = MenuPages[id];
+
+            if (newPage != null && Detail != newPage)
+            {
+                Detail = newPage;
+
+                if (Device.RuntimePlatform == Device.Android)
+                    await Task.Delay(100);
+
+                IsPresented = false;
+            }
+        }
+    }
+}
